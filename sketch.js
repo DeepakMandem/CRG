db4c0eac5a9573618beec4fbe42d4482b45613e4
DeepakMandem <72325190+DeepakMandem@users.noreply.github.com>
2020-12-24 20:09:25 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var canvas, backgroundImage;
-var car1,car2,car3,car4,cars;
 
 var gameState = 0;
 var playerCount;
@@ -9,9 +8,22 @@ var database;
 
 var form, player, game;
 
+var cars, car1, car2, car3, car4;
+var ground,track;
+
+function preload(){
+  car1_img=loadImage("car1.png");
+  car2_img=loadImage("car2.png");
+  car3_img=loadImage("car3.png");
+  car4_img=loadImage("car4.png");
+
+  ground=loadImage("ground.png");
+  track=loadImage("track.jpg");
+  
+}
 
 function setup(){
-  canvas = createCanvas(displayWidth-20,displayHeight-30);
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -26,5 +38,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
